dea6e86229d61ee8676ec0400ee9b1d87357b70c
Kenia25Soares <keniadefreitasmorais@gmail.com>
2025-10-20 15:23:14 +0100

    Class 28, Blade and Tailwind Techniques for Your Laravel Views

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,18 +1,18 @@
-<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
-            <div class="self-start text-sm">Laracasts</div>
+<x-panel class="flex flex-col text-center">
+    <div class="self-start text-sm">Laracasts</div>
 
-            <div class="py-8 font-bold">
-                <h3>Video Producer</h3>
-                <p>Full Time - From $60,000</p>
-            </div>
-
-           <div class="flex justify-between items-center mt-auto"> 
-             <div>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-            </div>
-
-                <img src="https://via.placeholder.com/42x42/1e40af/ffffff?text=42x42" alt="" class="w-[42px] h-[42px] rounded-xl border border-white/20">
-        </div>
+     <div class="py-8">
+        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">Video Producer</h3>
+        <p class="text-sm mt-4">Full Time - From $60,000</p>
     </div>
+
+    <div class="flex justify-between items-end mt-auto"> 
+        <div>
+        <x-tag size="small">Backend</x-tag>
+        <x-tag size="small">Frontend</x-tag>
+        <x-tag size="small">Manager</x-tag>
+    </div>
+
+        <x-employer-logo :width="42" />
+    </div>
+</x-panel>
